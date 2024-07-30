4f7c718c7308fe4541f56b8ed065de6ce02f6ced
Ericacdev <nechipato@gmail.com>
2024-07-29 17:55:03 -0700

    file

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
                 "task": to.get("title"),
                 "completed": to.get("completed"),
                 "username": user.get("username")
-            } for to in requests.get(url + "todos",
+                } for to in requests.get(url + "todos",
                                     params={"userId": user.get("id")}).json()]
             for user in users}, jsonfile)
