97d8b3a35b68d55f0e0103bc33fd4b55d1a2c5c5
Ericacdev <nechipato@gmail.com>
2024-09-26 12:32:04 -0700

    updated file

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,12 @@
 #increase file limits of  holberton user
 
 
-exec { 'limit increase file1':
-  command => 'sed -i s/'nofile 5'/'nofile 100'/g /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+exec { 'limit increase file_conf':
+  command => "sed -i s/'nofile 5'/'nofile 100'/g /etc/security/limits.conf",
+  path    => '/bin'
 }
 
-exec { 'limit increase file2':
-  command => 'sed -i s/'nofile 4'/'nofile 100'/g /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/'
+exec { 'limit increase fileconf_2':
+  command => "sed -i s/'nofile 4'/'nofile 100'/g /etc/security/limits.conf",
+  path    => '/bin'
 }
